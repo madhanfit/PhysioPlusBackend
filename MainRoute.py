5e484c3eb312b235e9a134dca88ab35c84cfcef8
A Chethan Reddy <69640722+chethanreddy123@users.noreply.github.com>
2023-08-01 22:28:19 +0530

    faster view

--- MainRoute.py
+++ MainRoute.py
@@ -17,7 +17,11 @@ import datetime
 from loguru import logger
 import reportgenerator
 from reportgenerator import create_pdf
-
+from fastapi import BackgroundTasks, FastAPI
+from fastapi.responses import StreamingResponse
+from pymongo import MongoClient
+from typing import List, Optional
+import json
 from datetime import date
 
 Key_Mongo_Cloud = "mongodb://aioverflow:12345@ac-pu6wews-shard-00-00.me4dkct.mongodb.net:27017,ac-pu6wews-shard-00-01.me4dkct.mongodb.net:27017,ac-pu6wews-shard-00-02.me4dkct.mongodb.net:27017/?ssl=true&replicaSet=atlas-jcoztp-shard-0&authSource=admin&retryWrites=true&w=majority"
@@ -87,6 +91,53 @@ def convert_to_second_json_format(first_json):
         "PARQ_Assessment": first_json["PARQ_Assessment"]
     }
     return second_json
+
+# Utility function to remove _id field from documents
+def remove_id(documents: List[dict]) -> List[dict]:
+    return [{**doc, "_id": None} for doc in documents]
+
+# Background job to process the data and generate the JSON response
+def process_patients(background_response: BackgroundTasks):
+    Find = PatientData.find({})
+    Find = list(Find) if Find is not None else []
+    if len(list(Find)) == 0:
+        return None
+
+    Result = list(Find)
+    Result = remove_id(Result)
+
+    for i in Result:
+        LastAsses = i.get('Assessment', [])
+        Checker = len(LastAsses)
+        if Checker == 0:
+            i['LastAssessment'] = 'No Assessment'
+            i['Status'] = "Not Yet"
+        else:
+            i['LastAssessment'] = LastAsses[-1]
+            SeniorDoctorPrescription = i['LastAssessment'].get('SeniorDoctorPrescription', {})
+            TreatmentPrescription = SeniorDoctorPrescription.get('TreatmentPrescription', {})
+            if TreatmentPrescription != {}:
+                i['Status'] = "Completed"
+            else:
+                i['Status'] = "Partial"
+
+    return Result
+
+# Helper function to generate streaming JSON response
+def stream_generator(background_response: BackgroundTasks):
+    patients = process_patients(background_response)
+    if patients is None:
+        yield "[]"
+        return
+
+    yield "["
+    first = True
+    for patient in patients:
+        if not first:
+            yield ","
+        yield json.dumps(patient)
+        first = False
+    yield "]"
 
 
 # --------------------------- Login Routes -----------------------------------
@@ -259,6 +310,21 @@ async def allPatients():
                     i['Status'] = "Not Yet"
         return {"allPatients" : Result}
     
+# Endpoint for retrieving paginated patients
+@app.get("/allPatientsFaster/", response_model=List[dict])
+async def all_patients(
+    skip: int = 0,
+    limit: int = 10
+):
+    background_response = BackgroundTasks()
+    background_response.add_task(process_patients, background_response)
+
+    return StreamingResponse(
+        stream_generator(background_response),
+        media_type="application/json"
+    )
+
+# Additional endpoints can be added here for filtering or specific patient retrieval
 
 @app.post("/updatePatient")
 async def updatePatient(info : Request):
